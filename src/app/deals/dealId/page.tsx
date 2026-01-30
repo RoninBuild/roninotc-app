58d73257a5cd68e53aa2d49b401e41793486a69d
RoninBuild <zmsmaxim@gmail.com>
2026-01-31 00:24:52 +0500

    Remove duplicate root app folder

---
+++ src/app/deals/dealId/page.tsx
@@ -0,0 +1,7 @@
+export default function DealPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl text-white">Deal Page Works!</h1>
+    </div>
+  )
+}
